2adaaed36eea29a6a0fa0aca4ea27a7125eaa18c
Neroware <neroware@freenet.de>
2017-04-29 21:45:53 +0200

    Add files via upload

---
+++ gui_inventory.gd
@@ -0,0 +1,227 @@
+
+extends Node2D
+
+var input_states = preload("res://Scripts/input_states.gd")
+
+var btn_up = input_states.new("btn_up")
+var btn_down = input_states.new("btn_down")
+var btn_right = input_states.new("btn_right")
+var btn_left = input_states.new("btn_left")
+var btn_action = input_states.new("btn_action")
+
+var item_container
+var current_page = 1
+var current_group_pages = 1
+var current_group = "Food"
+var active = false
+var current_selection = 3
+
+
+func _ready():
+	item_container = get_node("Item_Container")
+	set_fixed_process(true)
+
+
+func _fixed_process(delta):
+	if(is_active() == true):
+		check_btn_input()
+		if(get_node("GUI_Item_Info").is_active() == true):
+			active = false
+	get_node("Container_Sprite/Label 2").set_text(current_group)
+
+
+### Adds an item to the list
+func add_item(par1Item):
+	if(item_container.get_node(par1Item.get_type()) != null):
+		item_container.get_node(par1Item.get_type()).add_child(par1Item)
+	else:
+		print("!!Item has an unknown group!!")
+
+
+### Draws the item list in the inventory section of the GUI
+func setup_item_list():
+	clean_item_list()
+	
+	current_group_pages = ceil(float(item_container.get_node(current_group).get_children().size()) / 9.0)
+	if(current_group_pages == 0):
+		get_node("Container_Sprite/Label3").set_text("1 / 1")
+	else:
+		get_node("Container_Sprite/Label3").set_text(str(current_page) + " / " + str(current_group_pages))
+	
+	var items = item_container.get_node(current_group).get_children()
+	var array_length = items.size()
+	
+	var i = 0
+	while(i + 9 * (current_page - 1) < array_length && i < 9):
+		var index = i + 9 * (current_page - 1)
+		var item = items[index]
+		if(i == 0 || i == 1 || i == 2):
+			item.set_sprite_pos(Vector2(45 * (i - 1), -30))
+		elif(i == 3 || i == 4 || i == 5):
+			item.set_sprite_pos(Vector2(45 * (i - 4), 15))
+		elif(i == 6 || i == 7 || i == 8):
+			item.set_sprite_pos(Vector2(45 * (i - 7), 60))
+		i += 1
+
+
+### Totally cleans the drawed item list of the GUI
+func clean_item_list():
+	for ic in item_container.get_children():
+		for i in ic.get_children():
+			i.set_sprite_pos(Vector2(-10000, -10000))
+
+
+### Sets the GUI active or inactive
+func set_active(par1Active):
+	if(par1Active == false):
+		current_selection = 3
+	setup_item_list()
+	active = par1Active
+
+
+### Returns True of the GUI is active
+func is_active():
+	return active
+
+
+### Sets current item page
+func set_current_page(par1Page):
+	current_page = par1Page
+
+
+### Returns current item page
+func get_current_page():
+	return current_page
+
+
+### Sets current item group in the inventory section
+func set_current_group(par1Group):
+	current_group = par1Group
+
+
+### Returns current item group in the inventory section
+func get_current_group():
+	return current_group
+
+
+### Goes to the next item group
+func next_group():
+	current_page = 1
+	current_group_pages = null
+	if(current_group == "Weapons"):
+		current_group = "Armour"
+	elif(current_group == "Armour"):
+		current_group = "Food"
+	elif(current_group == "Food"):
+		current_group = "Quest"
+	elif(current_group == "Quest"):
+		current_group = "Others"
+	elif(current_group == "Others"):
+		current_group = "Weapons"
+	else:
+		current_group = "Weapons"
+
+
+### Goes to the previous item group
+func last_group():
+	current_page = 1
+	current_group_pages = null
+	if(current_group == "Weapons"):
+		current_group = "Others"
+	elif(current_group == "Others"):
+		current_group = "Quest"
+	elif(current_group == "Quest"):
+		current_group = "Food"
+	elif(current_group == "Food"):
+		current_group = "Armour"
+	elif(current_group == "Armour"):
+		current_group = "Weapons"
+	else:
+		current_group = "Weapons"
+
+
+### Checks the entire button input of the GUI
+func check_btn_input():
+	for i in get_node("Container_Sprite/Borders").get_children():
+		i.set_opacity(0.0)
+	get_node("Container_Sprite/Borders").get_children()[current_selection - 1].set_opacity(0.5)
+	
+	if(btn_right.check() == 1):
+		if(current_selection == 13):
+			current_selection = 1
+		else:
+			current_selection += 1
+	if(btn_left.check() == 1):
+		if(current_selection == 1):
+			current_selection = 13
+		else:
+			current_selection -= 1
+	if(btn_down.check() == 1):
+		if(current_selection == 1 || current_selection == 2):
+			current_selection = 3
+		elif(current_selection == 12 || current_selection == 13):
+			current_selection = 1
+		elif(current_selection == 3 || current_selection == 4 || current_selection == 5 || current_selection == 6 || current_selection == 7 || current_selection == 8):
+			current_selection += 3
+		else:
+			current_selection = 12
+	if(btn_up.check() == 1):
+		if(current_selection == 1 || current_selection == 2):
+			current_selection = 12
+		elif(current_selection == 12 || current_selection == 13):
+			current_selection = 9
+		elif(current_selection == 6 || current_selection == 7 || current_selection == 8 || current_selection == 9 || current_selection == 10 || current_selection == 11):
+			current_selection -= 3
+		else:
+			current_selection = 1
+	if(btn_action.check() == 1):
+		if(current_selection == 1):
+			last_group()
+			setup_item_list()
+			current_selection = 1
+		elif(current_selection == 2):
+			next_group()
+			setup_item_list()
+			current_selection = 2
+		elif(current_selection == 13):
+			if(current_page + 1 <= current_group_pages):
+				current_page += 1
+				setup_item_list()
+				current_selection = 13
+		elif(current_selection == 12):
+			if(current_page - 1 >= 1):
+				current_page -= 1
+				setup_item_list()
+				current_selection = 12
+		elif(current_selection == 3 || current_selection == 4 || current_selection == 5 || current_selection == 6 ||
+		     current_selection == 7 || current_selection == 8 || current_selection == 9 || current_selection == 10 ||
+		     current_selection == 11):
+			if(item_container.get_node(current_group).get_child((current_selection - 3) + 9 * (current_page - 1)) != null):
+				get_node("GUI_Item_Info").activate(item_container.get_node(current_group).get_child((current_selection - 3) + 9 * (current_page - 1)))
+				active = false
+
+
+### Sets a weapon in the weapon slot
+func equipe_weapon(par1Weapon):
+	if(get_node("/root/Game/Game_Node0/Stats_Control").get_weapon_slot() != null):
+		var old_weapon = get_node("/root/Game/Game_Node0/Stats_Control").get_weapon_slot()
+		get_node("../GUI_Stats/Container_Sprite/Equipped_Items/Slot_Weapon").remove_child(old_weapon)
+		get_node("Item_Container/Weapons").add_child(old_weapon)
+	
+	get_node("Item_Container/Weapons").remove_child(par1Weapon)
+	get_node("../GUI_Stats/Container_Sprite/Equipped_Items/Slot_Weapon").add_child(par1Weapon)
+	par1Weapon.set_sprite_pos(Vector2(33, -6))
+	setup_item_list()
+
+
+### Sets an armour in the weapon slot
+func equipe_armour(par1Armour):
+	if(get_node("/root/Game/Game_Node0/Stats_Control").get_armour_slot() != null):
+		var old_armour = get_node("/root/Game/Game_Node0/Stats_Control").get_armour_slot()
+		get_node("../GUI_Stats/Container_Sprite/Equipped_Items/Slot_Armour").remove_child(old_armour)
+		get_node("Item_Container/Armour").add_child(old_armour)
+	
+	get_node("Item_Container/Armour").remove_child(par1Armour)
+	get_node("../GUI_Stats/Container_Sprite/Equipped_Items/Slot_Armour").add_child(par1Armour)
+	par1Armour.set_sprite_pos(Vector2(33, 35))
+	setup_item_list()
---
+++ item.gd
@@ -0,0 +1,120 @@
+
+extends Node
+
+var item_type = "undefiend"
+var name = "..."
+var consumable = false
+var restores_hp = 0
+var damage = 0
+var defense = 0
+var special_effect = "none"
+var descr = "This is an item"
+var custom_usage_descr = "none"
+
+var base_texture = preload("res://Assets/Textures/items/base.png")
+
+var sprite
+
+
+func _ready():
+	sprite = get_node("Sprite")
+	set_sprite_pos(Vector2(-10000, -10000))
+	#use_base_texture()
+
+
+func get_sprite_textr():
+	return sprite.get_texture()
+
+
+func set_sprite_textr(par1Textr):
+	sprite = get_node("Sprite")
+	sprite.set_texture(par1Textr)
+
+
+func set_sprite_pos(par1Pos):
+	sprite.set_pos(par1Pos)
+
+
+func get_sprite_pos():
+	return sprite.get_pos()
+
+
+func set_type(par1Type):
+	item_type = par1Type
+
+
+func get_type():
+	return item_type
+
+
+func set_consumable(par1Consumable):
+	consumable = par1Consumable
+
+
+func get_consumable():
+	return consumable
+
+
+func set_restored_hp(par1Hp):
+	restores_hp = par1Hp
+
+
+func get_restored_hp():
+	return restores_hp
+
+
+func set_attack_damage(par1Dmg):
+	damage = par1Dmg
+
+
+func get_attack_damage():
+	return damage
+
+
+func set_defense(par1Defense):
+	defense = par1Defense
+
+
+func get_defense():
+	return defense
+
+
+func set_special_effect(par1Effect):
+	special_effect = par1Effect
+
+
+func get_special_effect():
+	return special_effect
+
+
+func set_description(par1Descr):
+	descr = par1Descr
+
+
+func get_description():
+	return descr
+
+
+func use_base_texture():
+	sprite.set_texture(base_texture)
+
+
+func set_name(par1Name):
+	name = par1Name
+
+
+func get_name():
+	return name
+
+
+func set_custom_usage_descr(par1Descr):
+	custom_usage_descr = par1Descr
+
+
+func get_custom_usage_descr():
+	return custom_usage_descr
+
+
+func use():
+	if(consumable == true):
+		get_parent().remove_child(self)
